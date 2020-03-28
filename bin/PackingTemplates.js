65938bf6395d465d0978f6b6c76a252db149176c
Michel Maia <dimermichel@gmail.com>
2020-03-28 04:24:42 -0400

    Add PackingTemplates.js

---
+++ bin/PackingTemplates.js
@@ -0,0 +1,354 @@
+let data = [
+  {
+    title: "Airplane",
+    items: ["Boarding pass",
+            "Download Netflix Movie",
+            "Books",
+            "Eye Mask",
+            "Ear Plugs",
+            "Headphones",
+            "Neck Pillow",
+            "Passport / ID"
+          ]
+  },
+  {
+    title: "Baby",
+    items: ["Baby Birth Certificate / Passport",
+            "Baby Bottle & Nipples",
+            "Baby Car Seat",
+            "Baby Clothes",
+            "Baby Food",
+            "Baby Front Carrier",
+            "Baby Hat",
+            "Baby Lotion",
+            "Baby Pain Reliever Pills",
+            "Baby Shoes",
+            "Baby Soap",
+            "Baby Sock",
+            "Baby Sunscreen",
+            "Baby Wipes",
+            "Blankets",
+            "Breastpump",
+            "Buggy",
+            "Changing Mat",
+            "Collapsible Stroller",
+            "Diaper Bag",
+            "Diapers",
+            "Hand Sanitizer",
+            "Health Book",
+            "Inflatable Baby Bathtub",
+            "Night Light",
+            "Stuffed Toys",
+            "Tissues",
+            "Toys",
+            "Water And/Or Juice"
+          ]
+  },
+  {
+    title: "Beach",
+    items: ["After Sun",
+            "Beach Bag",
+            "Beach Towel",
+            "Beach Umbrella",
+            "Hat",
+            "Sandals",
+            "Sunglasses",
+            "Sunscreen",
+            "Beach Swimsuit"
+          ]
+  },
+  {
+    title: "Boat",
+    items: ["Boat Tickets",
+            "Download Netflix Movie",
+            "Books",
+            "Seesickness Pills",
+            "Passport / ID"
+          ]
+  },
+  {
+    title: "Bus",
+    items: ["Buss Tickets",
+            "Download Netflix Movie",
+            "Books",
+            "Roadsickness Pills",
+            "Passport / ID"
+          ]
+  },
+  {
+    title: "Business",
+    items: ["Cufflinks",
+            "Work Shirt",
+            "Shoe Polish",
+            "Suit Jacket",
+            "Formal Watch",
+            "Tie",
+            "Work Belt",
+            "Work Pants",
+            "Work Shoes"
+          ]
+  },
+  {
+    title: "Camping",
+    items: ["Cooking Tools",
+            "Camping Food",
+            "Flashlight",
+            "Headlamp",
+            "Handheld GPS",
+            "Lighter/Matches",
+            "Pillow",
+            "Eating Utensils",
+            "Cooking Tools",
+            "Plastic Bags",
+            "Sleeping Bag",
+            "Sleeping Pad",
+            "Snacks",
+            "Tent",
+            "Toilet Paper",
+            "Towel",
+            "Utility Knife",
+            "Water Filter"
+          ]
+  },
+  {
+    title: "Car",
+    items: ["Car keys",
+            "Car papers",
+            "Driver's License",
+            "Check Tires"
+          ]
+  },
+  {
+    title: "Clothes",
+    items: ["Belt",
+            "Casual Pants",
+            "Casual Shirts",
+            "Light Jacket",
+            "Heavy Coat",
+            "Pajamas",
+            "Shoes",
+            "Socks",
+            "Sweater",
+            "Underwear",
+          ]
+  },
+  {
+    title: "Cruise",
+    items: ["Cruise Tickets",
+            "Download Netflix Movie",
+            "Books",
+            "Seasickness pills",
+            "Passport / ID"
+          ]
+  },
+  {
+    title: "Essentials",
+    items: ["Condoms",
+            "Headache pills",
+            "House Key",
+            "Medication",
+            "Powerbank",
+            "Printed Trip Itinerary",
+            "Reading Glasses",
+            "Smartphone",
+            "Smartphone charger",
+            "Umbrella",
+            "Vitamins",
+            "Wallet",
+            "Watch"
+          ]
+  },
+  {
+    title: "Family / Friends",
+    items: ["Gifts"]
+  },
+  {
+    title: "Fitness",
+    items: ["Gym Shoes",
+            "Gym Shorts ",
+            "Gym Socks",
+            "Gym T-shirt",
+            "Gym Towel",
+            "Headphones",
+            "Water Bottle",
+            "Padlock"
+          ]
+  },
+  {
+    title: "Hiking",
+    items: ["Backpack",
+            "Binoculars",
+            "Bug Spray",
+            "Compass",
+            "Sunscreen",
+            "Sunglasses",
+            "First Aid Kit",
+            "Hiking Shoes",
+            "Water Bottle"
+          ]
+  },
+  {
+    title: "Hotel",
+    items: ["Cruise",
+            "Seasickness",
+            "Passport / ID"
+          ]
+  },
+  {
+    title: "Hotel",
+    items: ["Reservation"]
+  },
+  {
+    title: "International",
+    items: ["Foreign Currency",
+            "Medical Insurance Card",
+            "Passport / ID",
+            "Pen",
+            "Power Adapter",
+            "Travel guide",
+            "Check Electronics Voltages",
+            "Vaccination Certificates",
+            "Visa"
+          ]
+  },
+  {
+    title: "Motorcycle",
+    items: ["Ear Plugs",
+            "Helmet",
+            "Motorcycle Boots",
+            "Motorcycle GPS/Smartphone Mount",
+            "Motorcycle Gloves",
+            "Motorcycle Jacket",
+            "Motorcycle Pants",
+            "Motorcycle Papers",
+            "Rain Gear",
+            "Shield Cleaner",
+            "Microfiber Towels",
+            "Toolbox",
+            "Tire Repair Kit",
+            "Hard Case For Gadgets",
+            "Waterproof Pouch"
+          ]
+  },
+  {
+    title: "Photography",
+    items: ["Batteries",
+            "Battery Charger",
+            "Camera",
+            "Camera Bag",
+            "Camera Charger",
+            "Lenses",
+            "Memory Cards",
+            "Tripod"
+          ]
+  },
+  {
+    title: "Rental",
+    items: ["Reservation",
+            "Sheets"
+          ]
+  },
+  {
+    title: "Snow Sports",
+    items: ["Lip Balm",
+            "Long Underwear",
+            "Long-sleeved Shirt",
+            "Scarf",
+            "Helmet",
+            "Ski Boots",
+            "Ski Poles",
+            "Skis",
+            "Skin Moisturizer",
+            "Snowboard Boots",
+            "Snowboard",
+            "Snow Gear Rental",
+            "Snow Gloves",
+            "Snow Hat/Beanie",
+            "Snow Goggles",
+            "Snow Jacket",
+            "Snow Pants",
+            "Winter Boots"
+          ]
+  },
+  {
+    title: "Swimming",
+    items: ["Goggles",
+            "Swimming Cap",
+            "Swimsuit",
+            "Sandals"
+          ]
+  },
+  {
+    title: "Todo List",
+    items: ["Book Flight",
+            "Book Hotel",
+            "Close all windows",
+            "Close water/gas faucets",
+            "Empty camera memory cards",
+            "Reduce thermostat",
+            "Take out the trash",
+            "Unplug electric devices",
+            "Water the plants"
+          ]
+  },
+  {
+    title: "Personal Care",
+    items: ["Band-Aids",
+            "Cologne",
+            "Contacts",
+            "Contacts solution",
+            "Cotton Swabs",
+            "Deodorant",
+            "Electric Shaver",
+            "Hairbrush",
+            "Nail Clippers",
+            "Razor",
+            "Shampoo",
+            "Shaving Gel",
+            "Shower Gel",
+            "Toothbrush",
+            "Toothpaste",
+            "Floss",
+            "Tweezers",
+            "Qtips"
+          ]
+  },
+  {
+    title: "Train",
+    items: ["Train Tickets",
+            "Download Netflix Movie",
+            "Books",
+            "Passport / ID"
+          ]
+  },
+  {
+    title: "Work",
+    items: ["Business Cards",
+            "VPN Keyfob",
+            "Laptop",
+            "Laptop Charger",
+            "Work Badge",
+            "Work Phone",
+            "Work Phone Charger"
+          ]
+  }
+]
+
+const reformattedObj = data.map(obj => {
+  const finalObj = {}
+  finalObj.title = obj.title
+  const nObj = Object.assign(obj.items)
+  const newObj = nObj.map((el) => {
+    const obj = {}
+    obj.name = el
+    obj.quantity = 1
+    obj.checked = false
+    return obj
+  })
+  finalObj.items = newObj
+  finalObj.checked = false
+  return finalObj
+})
+console.table(reformattedObj)
+
+module.exports = reformattedObj
