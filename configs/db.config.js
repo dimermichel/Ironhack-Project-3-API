3863ba1d5abe9954a64996b01a348a58fb4089ca
Michel Maia <dimermichel@gmail.com>
2020-04-27 13:24:01 -0400

    Change DB config to deployment

--- configs/db.config.js
+++ configs/db.config.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 mongoose
-  .connect('mongodb://localhost/ironhack-project-3-api', {
+  .connect(process.env.MONGODB_URI, {
     useCreateIndex: true,
     useNewUrlParser: true,
     useUnifiedTopology: true
